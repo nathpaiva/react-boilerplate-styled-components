f7f03ff8001e9bcfe50a59b4f9acd0bc345e89ca
Nath Paiva <hello@nathpaiva.com.br>
2018-08-17 18:07:12 -0300

    Add a husky

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 'user strict';
 
-import React, { Component } from 'react';
+import React from 'react'
 import Box from './components/box';
 import './css/reset.css';
 
-class App extends Component {
-  render() {
-    return (<Box title='My title.' text='My sub title.' />);
-  }
-}
+const App = () => <Box title='My title.' text='My sub title.' />;
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 import App from './App';
 
-const renderApp = (NextApp) => {
+const renderApp = NextApp => {
   render(
     <AppContainer>
       <NextApp />
